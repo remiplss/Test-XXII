1fc0d2457d680704a33b0eaa9d7bc4884c73b1ce
remiplss <57995218+remiplss@users.noreply.github.com>
2022-02-01 19:13:22 +0100

    ajout HomePage

---
+++ src/HomePage/HomePage.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+import { userActions } from '../_actions';
+
+class HomePage extends React.Component {
+    componentDidMount() {
+        this.props.getUsers();
+    }
+
+    handleDeleteUser(id) {
+        return (e) => this.props.deleteUser(id);
+    }
+
+    render() {
+        const { user, users } = this.props;
+        return (
+            <div className="col-md-6 col-md-offset-3">
+                <h1>Hi {user.firstName}!</h1>
+                
+                <button className="btn btn-primary">Add a video</button>
+
+                <p>
+                    <Link to="/User">User</Link>
+                </p>
+                <p>
+                    <Link to="/login">Logout</Link>
+                </p>
+            </div>
+        );
+    }
+}
+
+function mapState(state) {
+    const { users, authentication } = state;
+    const { user } = authentication;
+    return { user, users };
+}
+
+const actionCreators = {
+    getUsers: userActions.getAll,
+    deleteUser: userActions.delete
+}
+
+const connectedHomePage = connect(mapState, actionCreators)(HomePage);
+export { connectedHomePage as HomePage };
